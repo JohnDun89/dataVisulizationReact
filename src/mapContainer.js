ebc1d958d3612f54207be81846bb26440fa826a2
John Duncan <john@johnduncanphoto.com>
2018-05-25 14:52:40 +0100

    map container componenet added

---
+++ src/mapContainer.js
@@ -0,0 +1,14 @@
+import DeckGL, { LineLayer } from "deck.gl";
+import React, { Component } from "react";
+
+class MapContainer extends Component {
+  render() {
+    return (
+      <div className="MapContainer">
+        <p>I am a map</p>
+      </div>
+    );
+  }
+}
+
+export default MapContainer;
